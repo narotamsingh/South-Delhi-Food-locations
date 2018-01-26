a3a6e66733a912ccf364b9cc32ac585cf17463a4
Narotam Singh <narotam.singh@gmail.com>
2018-01-26 17:18:31 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,185 @@
-window.onerror=function(e){console.log(e)};var filter=function(e,n){this.name=e,this.value=n};function initViewModel(e){ListViewModel=ko.applyBindings(new ListViewModel(e))}var map,ListViewModel=function(e){var n=this;n.venues=e,n.locations=ko.observableArray(),n.currentFilter=ko.observable(),n.filters=ko.observableArray([new filter("All","All"),new filter("Indian","Indian Restaurant"),new filter("Asian","Asian Restaurant"),new filter("Café","Café"),new filter("Diner","Diner"),new filter("Japanese","Japanese Restaurant"),new filter("French","French Restaurant"),new filter("Mexican","Mexican Restaurant"),new filter("Coffee","Coffee Shop"),new filter("Italian","Italian Restaurant"),new filter("Tea","Tea Room")]),n.filterLocations=ko.computed(function(){return n.currentFilter()?"All"===n.currentFilter().name?(console.log("All List"),deleteMarkers(),createMarkers(n.venues),n.locations(n.venues)):(console.log("List has been filtered to: "+n.currentFilter().value),deleteMarkers(),createMarkers(ko.utils.arrayFilter(n.venues,function(e){if(e.categories[0].name===n.currentFilter().value)return!0})),n.locations(ko.utils.arrayFilter(n.venues,function(e){if(e.categories[0].name===n.currentFilter().value)return!0}))):(console.log("All List"),deleteMarkers(),createMarkers(n.venues),n.locations(n.venues))})};function listItemClick(e){for(var n in markers)markers[n].setAnimation(null);for(n in infoWindows)infoWindows[n].close();var r;for(n in markers)e.name===markers[n].title&&(markers[n].setAnimation(google.maps.Animation.BOUNCE),r=markers[n]);var t=e.name,a=e.contact.formattedPhone,i=e.location.formattedAddress,o=e.stats.checkinsCount,s=e.url;a||(a="---"),i||(i="---");var l=new google.maps.InfoWindow({content:"<b>Name:</b> "+t+"<br><b>Phone:</b> "+a+"<br><b>Address:</b> "+i+"<br><b>Checkins:</b> "+o+"<br><a href='"+s+"' title='Web Site'>Visit Web Site</a>"});infoWindows.push(l),l.open(map,r)}var markers=[],infoWindows=[];function initMap(){map=new google.maps.Map(document.getElementById("map"),{center:{lat:28.592181,lng:77.219193},zoom:15,disableDefaultUI:!0})}function createMarkers(e){for(var n in e){var r=new google.maps.Marker({position:{lat:e[n].location.lat,lng:e[n].location.lng},map:map,animation:google.maps.Animation.DROP,title:e[n].name});attachInfo(r,e[n]),markers.push(r)}}function setMapOnAll(e){for(var n in markers)markers[n].setMap(e)}function clearMarkers(){setMapOnAll(null)}function deleteMarkers(){clearMarkers(),markers=[]}function attachInfo(e,n){var r=n.name,t=n.contact.formattedPhone,a=n.location.formattedAddress,i=n.stats.checkinsCount,o=n.url;t||(t="---"),a||(a="---"),o||(o="---");var s=new google.maps.InfoWindow({content:"<b>Name:</b> "+r+"<br><b>Phone:</b> "+t+"<br><b>Address:</b> "+a+"<br><b>Checkins:</b> "+i+"<br><a href='"+o+"' title='Web Site'>Visit Web Site</a>"});infoWindows.push(s),e.addListener("click",function(){for(var n in markers)markers[n].setAnimation(null);for(n in infoWindows)infoWindows[n].close();s.open(map,e),e.setAnimation(google.maps.Animation.BOUNCE)})}
+window.onerror = function(e){
+    console.log(e);
+};
+
+// Model for Filter
+var filter = function (name, value) {
+    this.name = name;
+    this.value = value;
+};
+
+// Controller
+function initViewModel(venues) {
+    // Initialize List View
+    ListViewModel = ko.applyBindings(new ListViewModel(venues));
+}
+
+// List View Model
+var ListViewModel = function (venues) {
+    var self = this;
+    // Array of venues from foursquare api call
+    self.venues = venues;
+    // Binded Data for list view of locations
+    self.locations = ko.observableArray();
+    // Currently Selected filter from UI
+    self.currentFilter = ko.observable();
+    // List of filters
+    self.filters = ko.observableArray([
+                                new filter("All", "All"),
+                                new filter("Indian", "Indian Restaurant"),
+                                new filter("Asian", "Asian Restaurant"),
+                                new filter("Café", "Café"),
+                                new filter("Diner", "Diner"),
+                                new filter("Japanese", "Japanese Restaurant"),
+                                new filter("French", "French Restaurant"),
+                                new filter("Mexican", "Mexican Restaurant"),
+                                new filter("Coffee", "Coffee Shop"),
+                                new filter("Italian", "Italian Restaurant"),
+                                new filter("Tea", "Tea Room")]);
+    // Filtered Locations and Passes Array to locations observable
+    self.filterLocations = ko.computed(function () {
+        if (!self.currentFilter()) {
+            console.log("All List");
+            deleteMarkers();
+            createMarkers(self.venues);
+            return self.locations(self.venues);
+        } else if (self.currentFilter().name === "All") {
+            console.log("All List");
+            deleteMarkers();
+            createMarkers(self.venues);
+            return self.locations(self.venues);
+        } else {
+            console.log("List has been filtered to: " + self.currentFilter().value);
+            deleteMarkers();
+            createMarkers(ko.utils.arrayFilter(self.venues, function (venue) {
+                if (venue.categories[0].name === self.currentFilter().value) {
+                    return true;
+                }
+            })
+            );
+            return self.locations(
+                ko.utils.arrayFilter(self.venues, function (venue) {
+                    if (venue.categories[0].name === self.currentFilter().value) {
+                        return true;
+                    }
+                })
+            );
+        }
+    });
+};
+
+// List Item Click Event
+function listItemClick(location) {
+    for (var i in markers) {
+            markers[i].setAnimation(null);
+        }
+    for (i in infoWindows) {
+        infoWindows[i].close();
+    }
+    var marker;
+    for (i in markers) {
+        if (location.name === markers[i].title) {
+            markers[i].setAnimation(google.maps.Animation.BOUNCE);
+            marker = markers[i];
+        }
+    }
+    var name = location.name;
+    var phone = location.contact.formattedPhone;
+    var address = location.location.formattedAddress;
+    var count = location.stats.checkinsCount;
+    var url = location.url;
+    if (!phone) {
+        phone = "---";
+    }
+    if (!address) {
+        address = "---";
+    }
+    var infowindow = new google.maps.InfoWindow({
+      content:"<b>Name:</b> " + name + "<br>" +
+              "<b>Phone:</b> " + phone + "<br>" +
+              "<b>Address:</b> " + address + "<br>" +
+              "<b>Checkins:</b> " + count + "<br>" +
+              "<a href='" + url + "' title='Web Site'>Visit Web Site</a>"
+    });
+    infoWindows.push(infowindow);
+    infowindow.open(map, marker);
+}
+
+// Google Map-Initialize 
+var map;
+var markers = [];
+var infoWindows = [];
+function initMap() {
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat:28.592181, lng:77.219193},
+        zoom: 15,
+        disableDefaultUI:true
+    });
+
+}
+
+// Marker View Model
+function createMarkers(locations) {
+    for (var i in locations) {
+        var marker = new google.maps.Marker({
+            position: {lat:locations[i].location.lat, lng:locations[i].location.lng},
+            map:map,
+            animation:google.maps.Animation.DROP,
+            title:locations[i].name
+        });
+        attachInfo(marker, locations[i]);
+        markers.push(marker);
+    }
+}
+
+function setMapOnAll(map) {
+    for (var i in markers) {
+        markers[i].setMap(map);
+    }
+}
+
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+function attachInfo(marker, location) {
+    var name = location.name;
+    var phone = location.contact.formattedPhone;
+    var address = location.location.formattedAddress;
+    var count = location.stats.checkinsCount;
+    var url = location.url;
+
+    if (!phone) {
+        phone = "---";
+    }
+    if (!address) {
+        address = "---";
+    }
+    if (!url) {
+        url = "---";
+    }
+    var infowindow = new google.maps.InfoWindow({
+        content:"<b>Name:</b> " + name + "<br>" +
+                "<b>Phone:</b> " + phone + "<br>" +
+                "<b>Address:</b> " + address + "<br>" +
+                "<b>Checkins:</b> " + count + "<br>" +
+                "<a href='" + url + "' title='Web Site'>Visit Web Site</a>"
+    });
+    infoWindows.push(infowindow);
+    marker.addListener("click", function () {
+        for (var i in markers) {
+            markers[i].setAnimation(null);
+        }
+        for (i in infoWindows) {
+            infoWindows[i].close();
+        }
+        infowindow.open(map, marker);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    });
+}
